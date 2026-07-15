ee1b9299fabbcee995e2f7223496a8d646256457
Umar <umermujahid518@gmail.com>
2026-07-15 14:20:35 +0500

    Refactor: Add name Validation, spacing, and gitignore for local JSON data

--- contact_book.py
+++ contact_book.py
@@ -5,26 +5,28 @@ try:
         contacts = json.load(file)
 except FileNotFoundError:
     contacts = {}
-print("Console based Contact Book")
+
+print("\nConsole based Contact Book")
 
 while True:
-    print("1.Add Contact \n2.View All Contacts \n3.Search Contact \n4.Delete Contact \n5.Exit")
-    choice= input("Select an option: ")
+    print("\n1.Add Contact \n2.View All Contacts \n3.Search Contact \n4.Delete Contact \n5.Exit")
+    choice = input("Select an option: ")
+    print()  # Terminal par spacing ke liye
     
-    if choice== "1":
-        name= input("Enter Name: ")
-        while name.strip() == "":
-            print("Name can not be empty..!")
-            name= input("Enter Name: ")
+    if choice == "1":
+        name = input("Enter Name: ")
+        while name.strip() == "" or not name.replace(" ", "").isalpha():
+            print("Name can only contain letters and spaces!")
+            name = input("Enter Name: ")
             
         if name in contacts:
-            print(f"{name} already exists. Choose different name..!")
+            print(f"\n{name} already exists. Choose different name..!")
         else:
             phone = input("Enter phone number: ")
             
             while not phone.isdigit():
                 print("Only digits are allowed..!")
-                phone=input("Enter phone number: ")
+                phone = input("Enter phone number: ")
                 
             email = input("Enter email: ")
             
@@ -34,21 +36,22 @@ while True:
             
             name = name.strip()
             contacts[name] = {"phone": phone, "email": email}
-            print(f"{name} added successfully..!")
-            with open("contacts.json","w") as file:
+            print(f"\n{name} added successfully..!")
+            with open("contacts.json", "w") as file:
                 json.dump(contacts, file, indent=4)
             
     elif choice == "2":
-        print("Viewing all contacts: ")
+        print("Viewing all contacts:\n")
         if not contacts:
             print("No contacts found..!")
         for key, value in contacts.items():
-            print(f"{key} - Phone: {value['phone']}, Email: {value['email']}")
+            print(f"{key} - Phone: {value['phone']} - Email: {value['email']}")
     
-    elif choice=="3":
+    elif choice == "3":
         search_name = input("Enter name to search: ")
+        print()
         if search_name in contacts:
-            print(f"{search_name} - Phone: {contacts[search_name]['phone']}- Email: {contacts[search_name]['email']}")
+            print(f"{search_name} - Phone: {contacts[search_name]['phone']} - Email: {contacts[search_name]['email']}")
         else:
             print(f"{search_name} not in contacts")
             
@@ -56,15 +59,14 @@ while True:
         del_contact = input("Enter contact to delete: ")
         if del_contact in contacts:
             del contacts[del_contact]
-            print(f"{del_contact} deleted successfully..!")
-            with open("contacts.json","w") as file:
+            print(f"\n{del_contact} deleted successfully..!")
+            with open("contacts.json", "w") as file:
                 json.dump(contacts, file, indent=4)
-            
         else:
-            print(f"{del_contact} not found.. !")
+            print(f"\n{del_contact} not found.. !")
     
     elif choice == "5":
-        print("Thank you for using the System..!!")
+        print("Thank you for using the System..!!\n")
         break
     else:
         print("Invalid choice..! Try Again")
